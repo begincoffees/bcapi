be44cceed9ce98d14c839f9e5b8822cae2e0b0b3
Andrew Angelle <andrew.angelle@rackspace.com>
2018-12-01 01:14:52 -0600

    cleanup and vars

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,13 @@ const withDB = (req) => ({
   ...req,
   headers: {
     ...req.headers,
-    Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NDM2MzMwODUsIm5iZiI6MTU0MzU0NjY4NX0.fjmRfPkQr9kOI-fnWJlLJjN0l5JdvOUlZlXLb3b1md8`
+    Authorization: `Bearer ${process.env.PRISMA_TOKEN}`
   },
 
   db: new Prisma({
     typeDefs: './src/generated/prisma.graphql',
-    endpoint: `http://begincoffeesprisma.herokuapp.com/bcprisma/test`,
-    secret: `576eef8cb1f325292da28c44ff8c48237782409c`
+    endpoint: process.env.PRISMA_ENDPOINT,
+    secret: process.env.PRISMA_SECRET
   } as any)
 })
 
