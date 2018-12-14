0371647233f8e9b1eda45e5d92841ba8bef44d79
Andrew Angelle <andrew.angelle@rackspace.com>
2018-12-13 19:32:48 -0600

    k

---
+++ src/generated/tmp-resolvers/Invoice.ts
@@ -0,0 +1,30 @@
+// This resolver file was scaffolded by github.com/prisma/graphqlgen, DO NOT EDIT.
+// Please do not import this file directly but copy & paste to your application code.
+
+import { InvoiceResolvers } from "../graphqlgen";
+
+export const Invoice: InvoiceResolvers.Type = {
+  ...InvoiceResolvers.defaultResolvers,
+
+  items: (parent, args, ctx) => {
+    throw new Error("Resolver not implemented");
+  },
+  stripeRecord: (parent, args, ctx) => {
+    throw new Error("Resolver not implemented");
+  },
+  created: (parent, args, ctx) => {
+    throw new Error("Resolver not implemented");
+  },
+  stripePaymentId: (parent, args, ctx) => {
+    throw new Error("Resolver not implemented");
+  },
+  stripeCustomerId: (parent, args, ctx) => {
+    throw new Error("Resolver not implemented");
+  },
+  customer: (parent, args, ctx) => {
+    throw new Error("Resolver not implemented");
+  },
+  vendors: (parent, args, ctx) => {
+    throw new Error("Resolver not implemented");
+  }
+};
